ad7c0b55852cebdca84d22967d49f36ad88ae8f1
Trygve Kristiansen <trygvekristiansen13@gmail.com>
2024-09-09 15:03:11 +0200

    Exploring data first day

--- Scripts/data_import.R
+++ Scripts/data_import.R
@@ -1,3 +1,24 @@
 library(tidyverse)
 library(here)
-read_delim(here("DATA", "exam_dataset.txt"))
+library(skimr)
+library(naniar)
+
+mydata <- read_delim(here("DATA", "exam_dataset.txt"))
+mydata
+
+#initial exploration of data ----
+
+tail(mydata)
+head(mydata)
+view(mydata)
+mydata %>% count(id)
+glimpse(mydata)
+skimr::skim(mydata)
+naniar::gg_miss_var(mydata)
+
+
+# 602 "ids" are duplicated
+# two colomn names start with a number
+# one colomn name with a space
+# variable types wrong, some should be changed from character to "factor"
+# no missing values
