44dfda9f004834e5e82c63d72087560b58ae96d6
Silja79 <shanseth@gmail.com>
2024-09-09 15:03:45 +0200

    Day 1 exploring the data

--- Scripts/data_import.R
+++ Scripts/data_import.R
@@ -1,3 +1,19 @@
 library(tidyverse)
 library(here)
-read_delim(here("DATA", "exam_dataset.txt"))
+library("skimr")
+library("naniar")
+mydata <-read_delim(here("DATA", "exam_dataset.txt"))
+tail(mydata)
+tail(mydata, n=30)
+view("mydata")
+
+#or look in enviroment to se MyData, press arrow or open the file
+# coloumns 81 ASSA 325 ASA feature type need to be renamed
+#all variables are in coloumns 
+#all variables except age and risk should be factor
+
+skimr::skim("mydata")
+
+naniar::gg_miss_var("mydata")
+#see gg-plot for missing variables:there are none missing 
+
