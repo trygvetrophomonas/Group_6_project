c09ae860e6906255b52140d87facd1e49ff2fa4d
Trygve Kristiansen <trygvekristiansen13@gmail.com>
2024-09-12 13:40:32 +0200

    .

--- Scripts/Script_plot_trygve.R
+++ Scripts/Script_plot_trygve.R
@@ -6,7 +6,7 @@ library(naniar)
 library(ggplot2)
 mydata3 <- read_delim(here("DATA", "mydata_joined2024-09-12.txt"))
 
-# Does the age distribution of the patients depend on their sex (`gender`)???
+# Does the age distribution of the patients depend on their sex (`gender`)???-
 
 ggplot(data=mydata3) +
   aes(x= gender, 
